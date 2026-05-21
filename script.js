066e1417aa0b3e719b0e8c21f0ee14f2396e15ee
unknown <cedricprime9@gmail.com>
2026-05-21 03:38:56 -1100

    :

---
+++ script.js
@@ -0,0 +1,43 @@
+// let input = document.getElementById('screen');
+
+
+// function addvalue(value){
+//     input.innerHTML = value;
+//     console.log( input.innerHTML );
+    
+// }
+// function calculate (){
+//     let result =
+// eval(document.getElementById("screen").value);
+// document.getElementById("screen").value = result;
+
+// }
+// function emptyscreen(){
+// document.getElementById("screen").value = "";
+
+// }
+// function squareroot(){
+//     let number =
+//     document.getElementById("screen").value ;
+//     document.getElementById("screen").value = Math.sqrt(number);
+
+// }
+// function power(){
+//     let number =
+//     document.getElementById("screen").value ;
+//     document.getElementById("screen").value = Math.pow(number);
+
+// }
+// function sinvalue(){
+//     let number =
+//     document.getElementById("screen").value ;
+//     document.getElementById("screen").value = Math.sin(number);
+
+// }
+// function cosvalue(){
+//     let number =
+//     document.getElementById("screen").value ;
+//     document.getElementById("screen").value = Math.cos(number);
+
+// }
+
